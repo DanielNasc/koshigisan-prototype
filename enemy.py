a87efe24e7b82c6b39ca76450a5a1b37287190b1
Lonalt <luann.alves353@gmail.com>
2022-11-10 16:51:25 -0300

    modified:   enemy.py

--- enemy.py
+++ enemy.py
@@ -148,7 +148,7 @@ class Enemy(Entity):
 #--------------Lonalt--------
     def get_damage(self, player,attack_type):
         if self.vulnerable:
-            self.direction = self.get_player_distance_and_direction(player)[]
+            self.direction = self.get_player_distance_and_direction(player)[1]
             if attack_type == 'weapon':
                 self.health -= player.get_full_weapon_damage()
             else:
