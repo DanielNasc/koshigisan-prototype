b1c819630d9c18fc6c5b5fbfcf0b0ab68d09b7a1
Lonalt <luann.alves353@gmail.com>
2022-11-12 17:29:22 -0300

    modified:   upgrade.py

--- upgrade.py
+++ upgrade.py
@@ -3,8 +3,72 @@ from settings import *
 
 class Upgrade:
     def __init__(self,player):
+        #general setup
         self.display_surface = pygame.display.get_surface()
         self.player = player
+        self.attribute_nr = len(player.stats)
+        self.attribute_names = list(player.stats.keys())
+        self.font = pygame.font.Font(UI_FONT,UI_FONT_SIZE)
+
+        #item creation
+        self.height = self.display_surface.get_size()[1] * 0.8
+        self.width = self.display_surface.get_size()[0] // 6
+        self.creat_items()
+
+        #selection system
+        self.selection_index = 0
+        self.selection_time = None
+        self.can_move = True
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+        if  self.can_move:
+            if keys[pygame.K_RIGHT] and self.selection_index < self.attribute_nr -1:
+                self.selection_index += 1
+                self.can_move = False
+                self.selection_time = pygame.time.get_ticks()
+
+            elif keys[pygame.K_LEFT] and self.selection_index > 1:
+                self.selection_index -= 1
+                self.can_move = False
+                self.selection_time = pygame.time.get_ticks()
+
+            if keys[pygame.K_SPACE]:
+                self.can_move = False
+                self.selection_time = pygame.time.get_ticks()
+                print(self.selection_index)
+
+    def selecion_cooldown(self):
+        if not self.can_move:
+            current_time = pygame.time.get_ticks()
+            if current_time - self.selection_time >= 300:
+                self.can_move = True
+
+    def creat_items(self):
+        self.item_list = []
+
+        for item, index in enumerate(range(self.attribute_nr)):
+            full_width = self.display_surface.get_size()[0]
+            increment = full_width // self.attribute_nr
+            left = (item * increment) + (increment - self.width) // 2
+
+            top = self.display_surface.get_size()[1] * 0.1
+
+            item = Item(left,top,self.width,self.height,index,self.font)
+            self.item_list.append(item)
 
     def display(self):
-        self.display_surface.fill('black')
+        self.input()
+        self.selecion_cooldown()
+
+        for item in self.item_list:
+            item.display(self.display_surface,0,'test',1,2,3)
+
+class Item:
+    def __init__(self,l,t,w,h,index,font):
+        self.rect = pygame.Rect(l,t,w,h)
+        self.index = index
+        self.font = font
+
+    def display(self,surface,selection_num,name, value,max_value,cost):
+        pygame.draw.rect(surface, UI_BG_COLOR,self.rect)
